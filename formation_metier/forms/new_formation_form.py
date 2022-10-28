6dfce1edb8751793bb3c310789751755596f6ad8
vbrichant <56791491+vbrichant@users.noreply.github.com>
2022-10-28 09:24:45 +0200

    Revert "detail/new/update_view"

--- formation_metier/forms/new_formation_form.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from formation_metier.models.formation import Formation
-
-
-class NewFormationForm(ModelForm):
-    class Meta:
-        model = Formation
-        fields = ('name', 'code', 'description')
