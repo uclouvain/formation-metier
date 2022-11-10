f69bbaa598b749ba7a83c0ba4f958264afd2bdfe
Vincent Brichant <he201583@students.ephec.be>
2022-11-10 15:18:15 +0100

    Update model RoleOsis : add unique constaint on field "name"

--- formation_metier/models/role_osis.py
+++ formation_metier/models/role_osis.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class RoleOsis(models.Model):
-    name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=100, null=False, unique=True)
