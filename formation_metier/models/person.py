65203261c0e4f7a2aa4f17b6be0037b48def4e4f
Vincent Brichant <he201583@students.ephec.be>
2022-11-17 16:40:33 +0100

    Création d'une Enums contenant les roles dans osis
    + intégration de l'enum dans le modèle Person et dans Session

--- formation_metier/models/person.py
+++ formation_metier/models/person.py
@@ -1,18 +1,11 @@
 from django.db import models
+from formation_metier.enums.roles_osis_enum import ROLES_OSIS_CHOICES
 
 
 class Person(models.Model):
-    ADMINISTRATEUR = 'Admin'
-    FORMATEUR = 'Form'
-    PARTICIPANT = 'Part'
-    ROLES = [
-        (ADMINISTRATEUR, 'Administrateurs'),
-        (FORMATEUR, 'Formateurs'),
-        (PARTICIPANT, 'Participant'),
-    ]
     name = models.CharField(max_length=50, blank=False)
     numberFGS = models.CharField(max_length=8, blank=False)
-    role_formation_metier = models.CharField(choices=ROLES, max_length=20, default=PARTICIPANT, blank=False)
+    role_formation_metier = models.CharField(choices=ROLES_OSIS_CHOICES, max_length=50, default=None, blank=True)
 
     def __str__(self):
         return f"{self.name}"
--- formation_metier/models/session.py
+++ formation_metier/models/session.py
@@ -2,18 +2,17 @@ import django.utils.timezone
 from django.db import models
 
 from formation_metier.models import formation
-from formation_metier.models.person import Person
+from formation_metier.enums.roles_osis_enum import ROLES_OSIS_CHOICES
 
 
 class Session(models.Model):
-
-    CHOICES_PUBLIC_CIBLE = Person.ROLES
+    CHOICES_PUBLIC_CIBLE = ROLES_OSIS_CHOICES
     formation = models.ForeignKey(formation.Formation, on_delete=models.CASCADE, blank=False)
     session_date = models.DateTimeField(default=django.utils.timezone.now, blank=False)
     local = models.CharField(max_length=50, blank=False)
     participant_max_number = models.IntegerField(default=0, blank=False)
     formateur_id = models.CharField(max_length=50, blank=False)
-    public_cible = models.CharField(max_length=50, choices=CHOICES_PUBLIC_CIBLE, default=Person.PARTICIPANT)
+    public_cible = models.CharField(max_length=50, choices=CHOICES_PUBLIC_CIBLE, default=None)
 
     def __str__(self):
         return "{} - {}".format(self.formation.name, str(self.session_date))
@@ -26,3 +25,6 @@ class Session(models.Model):
 
     def time_format(self):
         return self.session_date.__format__("%Hh%I")
+
+    def get_public_cible(self):
+        return self.CHOICES_PUBLIC_CIBLE[self.public_cible]
