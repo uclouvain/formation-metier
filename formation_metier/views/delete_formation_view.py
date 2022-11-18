fd54ba8dd0142e88033345aaac73de74a1458632
Vincent Brichant <he201583@students.ephec.be>
2022-11-18 15:52:29 +0100

    Utilisation de la librairie Typing + modification de la configuration de debug_toolbar

--- formation_metier/views/delete_formation_view.py
+++ formation_metier/views/delete_formation_view.py
@@ -1,9 +1,11 @@
+from typing import Union
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect
+from django.shortcuts import redirect
 from formation_metier.models.formation import Formation
 
 
-def delete_formation(request, formation_id):
+def delete_formation(request, formation_id) -> Union[HttpResponsePermanentRedirect, HttpResponseRedirect]:
     formation = Formation.objects.get(id=formation_id)
     formation.delete()
     messages.success(request, 'La formation {} a été supprimée.'.format(formation.name))
--- formation_metier/views/delete_registration_view.py
+++ formation_metier/views/delete_registration_view.py
@@ -1,11 +1,10 @@
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
 from formation_metier.models.register import Register
 
 
-def delete_registration(request, register_id):
+def delete_registration(request, register_id) -> HttpResponseRedirect:
     registration = Register.objects.get(id=register_id)
     url = reverse('formation_metier:detail_session', kwargs={'session_id': registration.session_id})
     registration.delete()
