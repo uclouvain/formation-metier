4e6fb1fb5ab3f5fc66dcde997a3e5a6784d0cf51
Vincent Brichant <he201583@students.ephec.be>
2022-11-21 13:31:53 +0100

    Ajout d'un input afin de recuperer le session_id dans le formulaire + Vérification que les inscription sont bien de la session actuelle

--- formation_metier/views/delete_registration_view.py
+++ formation_metier/views/delete_registration_view.py
@@ -2,18 +2,21 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect
 from formation_metier.models.register import Register
+from formation_metier.models.session import Session
 
 
 def delete_registration(request) -> HttpResponseRedirect:
     if request.method == "POST":
         register_list = request.POST.getlist('inscription')
         register_object_list = []
-        session_id = ""
+        session_id = request.POST.get("session_id")
+        session_register_set = Session.objects.get(id=session_id).register_set
         for register in register_list:
             register_object = get_object_or_404(Register, pk=register)
-            register_object_list.append(register_object)
-            if session_id == "":
-                session_id = register_object.session.id
+            if register_object not in session_register_set:
+                raise ValueError("l'inscription n'appartient pas a la session actuelle")
+            else:
+                register_object_list.append(register_object)
         for register_object in register_object_list:
             register_object.delete()
             messages.success(request, "L'inscription de l'utilisateur '{}' a été supprimée.".format(
