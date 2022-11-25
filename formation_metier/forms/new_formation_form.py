64647b59cfc17ee51b60131414ec8c8666728791
Vincent Brichant <he201583@students.ephec.be>
2022-11-25 13:43:23 +0100

    UpdateModel

--- formation_metier/forms/new_formation_form.py
+++ formation_metier/forms/new_formation_form.py
@@ -6,7 +6,7 @@ from formation_metier.models.formation import Formation
 class NewFormationForm(ModelForm):
     class Meta:
         model = Formation
-        fields = ('name', 'code', 'description')
+        fields = ('name', 'code', 'description', 'public_cible')
 
     # A voir si Charfield convertis automatiquement en Str
     def clean_name(self):
