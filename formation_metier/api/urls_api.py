3bfcebca502c07e4c731e8c95f89501651062f67
Vincent Brichant <he201583@students.ephec.be>
2022-11-29 13:48:01 +0100

    Update celery, API request+urls et indentation dans exemple_data_from_api.py

--- formation_metier/api/urls_api.py
+++ formation_metier/api/urls_api.py
@@ -2,6 +2,6 @@ from django.urls import path
 from formation_metier.api import views
 
 urlpatterns = [
-    path('employeucl/', views.EmployeUCLList.as_view()),
-    path('employeucl/<int:pk>/', views.EmployeUCLDetail.as_view()),
+    path('employe_ucl/', views.EmployeUCLList.as_view()),
+    path('employe_ucl/<int:pk>/', views.EmployeUCLDetail.as_view()),
 ]
--- formation_metier/tasks.py
+++ formation_metier/tasks.py
@@ -20,76 +20,61 @@ logger = logging.getLogger(settings.DEFAULT_LOGGER)
 # Commande pour lancer Celery avec exécution du beat :  celery -A formation_metier worker -B -l INFO
 
 
-@celery_app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
-
-
 @celery_app.task
-def get_person_from_osis() -> List[Dict]:
+def get_employe_ucl_from_osis() -> List[Dict]:
     # En attendant de faire les vrai appel API
-    create_person_object_from_api_response(data_person)
+    create_employe_ucl_object_from_api_response(data_person)
     url = settings.API_PERSON_URL + "employeucl/"
     try:
         persons = requests.get(
             url,
             timeout=20
         )
-        for item in persons.json():
-            print(item)
         # a décommenter lorsque il y aura la vrai API
-        # create_person_object_from_api_response(persons.json())
+        # create_employe_ucl_object_from_api_response(persons.json())
         return persons.json()
     except Exception:
-        logger.info("[Synchronize person] An error occurred during fetching person from OSIS")
+        logger.info("[Synchronize person] An error occurred during fetching employe_ucl from OSIS")
         raise ValueError
 
 
 @celery_app.task()
-def get_specific_person_from_osis(person_id: str) -> List[Dict]:
-    url = settings.API_PERSON_URL + "employeucl/" + str(person_id)
+def get_specific_employe_ucl_from_osis(person_id: str) -> List[Dict]:
+    url = settings.API_PERSON_URL + "employe_ucl/" + str(person_id)
     try:
         person = requests.get(
             url,
-            timeout=20  # settings.REQUESTS_TIMEOUT or 20
+            timeout=20
         )
         return person.json()
     except Exception:
-        logger.info("[Synchronize person] An error occurred during fetching person from OSIS")
+        logger.info("[Synchronize person] An error occurred during fetching employe_ucl from OSIS")
         raise ValueError
 
 
-def create_person_object_from_api_response(person_list_json: list):
-    print("passé dans creation de person")
+def create_employe_ucl_object_from_api_response(person_list_json: list):
     if not person_list_json:
         raise AssertionError
     if type(person_list_json) != list:
         raise TypeError
     else:
         for person in person_list_json:
-
             name = str(person["firstname"]) + " " + str(person["lastname"])
             numbers_fgs = person["matric_fgs"]
             user_object = User.objects.filter(username=name)
-            print(name)
-            print(user_object)
             if not user_object:
                 user_object = User.objects.create_user(username=name, password="osis")
             if type(user_object) is QuerySet:
-                print(type(user_object[0]))
-                print(user_object[0])
                 person_object = EmployeUCLouvain(name=name,
                                                  numberFGS=numbers_fgs,
                                                  role_formation_metier=RoleFormationFareEnum.PARTICIPANT,
                                                  user=user_object[0]
                                                  )
             else:
-                print(type(user_object))
-                print(user_object)
                 person_object = EmployeUCLouvain(name=name,
                                                  numberFGS=numbers_fgs,
                                                  role_formation_metier=RoleFormationFareEnum.PARTICIPANT,
                                                  user=user_object
                                                  )
-            if not EmployeUCLouvain.objects.filter(numberFGS=person_object.numberFGS):
+            if not EmployeUCLouvain.objects.filter(numberFGS=person_object.number_fgs):
                 person_object.save()
