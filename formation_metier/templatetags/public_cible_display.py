aef2a514e4276e0ec412377d11ed23b9b8287563
Vincent Brichant <he201583@students.ephec.be>
2022-11-29 14:36:15 +0100

    Suppression d'un templateTag non utile + suppression d'une vérification déja efectuée par LoginRequiredMixin + suppression de la librairie HTMX déja présente dans les packages du module

--- formation_metier/templatetags/public_cible_display.py
+++
@@ -1,9 +0,0 @@
-from formation_metier.models.formation import Formation
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag
-def public_cible_display(formation: Formation):
-    return formation.get_public_cible_display()
