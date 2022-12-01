3c0769876571427fe1a8c43146c48295c58bd728
Vincent Brichant <he201583@students.ephec.be>
2022-12-01 17:44:49 +0100

    Modification de Delete formation

--- formation_metier/views/delete_formation_view.py
+++ formation_metier/views/delete_formation_view.py
@@ -1,8 +1,12 @@
 from typing import Union
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect
 from django.shortcuts import redirect
+from django.views import generic
+
 from formation_metier.models.formation import Formation
 
 
@@ -14,3 +18,16 @@ def delete_formation(request, formation_id) -> Union[HttpResponsePermanentRedire
     formation.delete()
     messages.success(request, 'La formation {} a été supprimée.'.format(formation.name))
     return redirect('formation_metier:list_formation')
+
+
+class DeleteFormation(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, generic.DeleteView):
+    permission_required = 'formation_metier.delete_formation'
+    model = Formation
+    # success_url = '/list_formation'
+
+    pk_url_kwarg = "formation_id"
+
+    def get_success_url(self):
+        success_message = f'La formation {self.object.name} a été supprimée.'
+        messages.success(self.request, success_message)
+        return '/list_formation'
