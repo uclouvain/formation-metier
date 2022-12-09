d645f07d3601092be5d6cd68be75287f3da1a844
Vincent Brichant <he201583@students.ephec.be>
2022-12-09 16:50:34 +0100

    Refactor : modification du nommage des formulaires d'inscription pour plus de compréhention

--- formation_metier/views/inscription_seance_pour_formateur_view.py
+++ formation_metier/views/inscription_seance_pour_formateur_view.py
@@ -1,18 +1,18 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import FormView
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic.detail import SingleObjectMixin
-from django.views.generic.edit import FormView
 
-from formation_metier.forms.nouvelle_inscription_par_participant_form import NouvelleInscriptionParParticipantForm
+from formation_metier.forms.nouvelle_inscription_par_formateur_form import NouvelleInscriptionParFormateurForm
 from formation_metier.models.seance import Seance
 
 
-class InscriptionParParticipantFormView(LoginRequiredMixin, PermissionRequiredMixin, SingleObjectMixin, FormView):
+class InscriptionSeancePourFormateurFormView(LoginRequiredMixin, PermissionRequiredMixin, SingleObjectMixin, FormView):
     permission_required = 'formation_metier.add_inscription'
     template_name = 'formation_metier/detail_seance.html'
-    form_class = NouvelleInscriptionParParticipantForm
+    form_class = NouvelleInscriptionParFormateurForm
     model = Seance
     context_object_name = "seance"
     pk_url_kwarg = 'seance_id'
@@ -30,28 +30,17 @@ class InscriptionParParticipantFormView(LoginRequiredMixin, PermissionRequiredMi
             'seance': self.get_object(),
         }
 
-    def get_initial(self):
-        return {'participant': self.request.user.employeuclouvain}
-
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        form.data._mutable = True
-        form.data['participant'] = self.request.user.employeuclouvain
-        form.data['seance'] = self.get_object()
-        form.data._mutable = False
-        return super().post(self, request, *args, **kwargs)
-
-    def form_valid(self, form):
+    def form_valid(self, form, *args, **kwargs):
         inscription = self.get_form().save(commit=False)
         inscription.seance = self.get_object()
         inscription.save()
         messages.success(
             self.request,
-            f"Le participant {form.data['participant'].name} a été ajouté."
+            f'Le participant {inscription.participant.name} a été ajouté.'
         )
         return redirect(self.get_success_url())
 
-    def form_invalid(self, form):
+    def form_invalid(self, form, *args, **kwargs):
         return render(
             self.request,
             self.template_name,
