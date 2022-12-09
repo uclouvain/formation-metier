533841a36ee9909f46ed6278403da6d90665cd55
Vincent Brichant <he201583@students.ephec.be>
2022-12-09 16:44:52 +0100

    Refactor : Fusion de la partie DetailView de detail_seance_view.py

--- formation_metier/views/detail_seance_par_participant_view.py
+++ formation_metier/views/detail_seance_par_participant_view.py
@@ -1,46 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.db.models import Count
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views import generic
 from django.views.generic.detail import SingleObjectMixin
-from django.views.generic.edit import FormMixin, FormView
+from django.views.generic.edit import FormView
 
 from formation_metier.forms.nouvelle_inscription_par_participant_form import NouvelleInscriptionParParticipantForm
 from formation_metier.models.seance import Seance
-
-
-class DetailSeanceForParticipant(LoginRequiredMixin, PermissionRequiredMixin, FormMixin, generic.DetailView):
-    permission_required = [
-        'formation_metier.view_seance',
-        'formation_metier.view_inscription'
-    ]
-    model = Seance
-    template_name = 'formation_metier/detail_seance.html'
-    context_object_name = "seance"
-    pk_url_kwarg = 'seance_id'
-    form_class = NouvelleInscriptionParParticipantForm
-
-    def get_context_data(self, **kwargs):
-        return {
-            **super().get_context_data(**kwargs),
-            'form': self.get_form()
-        }
-
-    def get_form_kwargs(self):
-        return {
-            **super().get_form_kwargs(),
-            'seance': self.get_object()
-        }
-
-    def get_queryset(self):
-        return super().get_queryset().filter(id=self.kwargs['seance_id']).prefetch_related(
-            'inscription_set',
-            'inscription_set__participant'
-        ).annotate(
-            inscription_count=Count('inscription'),
-        )
 
 
 class InscriptionParParticipantFormView(LoginRequiredMixin, PermissionRequiredMixin, SingleObjectMixin, FormView):
