b1bfbe27c7c9f1faecacf8c4c73289cd7de15047
Vincent Brichant <he201583@students.ephec.be>
2022-12-19 10:05:06 +0100

    Rename SelectionParticipantView -> SelectionParticipantAutoComplete

--- formation_metier/views/selection_participant_auto_complete.py
+++ formation_metier/views/selection_participant_auto_complete.py
@@ -5,7 +5,7 @@ from django.utils.html import format_html
 from formation_metier.models.employe_uclouvain import EmployeUCLouvain
 
 
-class SelectionParticipantView(LoginRequiredMixin, PermissionRequiredMixin, autocomplete.Select2QuerySetView):
+class SelectionParticipantAutoComplete(LoginRequiredMixin, PermissionRequiredMixin, autocomplete.Select2QuerySetView):
     permission_required = [
         'formation_metier.view_employeuclouvain',
         'formation_metier.access_to_formation_fare'
