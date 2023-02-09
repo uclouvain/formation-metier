275fa138615279d312312f4468081b9b4a9c3a16
Vincent Brichant <he201583@students.ephec.be>
2023-02-09 16:49:43 +0100

    Update post for inscription_formation_pour_participant_view.py

--- formation_metier/views/inscription_formation_pour_participant_view.py
+++ formation_metier/views/inscription_formation_pour_participant_view.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from django import forms
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -8,7 +10,6 @@ from django.views.generic.detail import SingleObjectMixin
 
 from formation_metier.models.formation import Formation
 from formation_metier.models.inscription import Inscription
-from formation_metier.models.seance import Seance
 
 
 class InscriptionFormationPourParticipantForm(forms.ModelForm):
@@ -17,7 +18,7 @@ class InscriptionFormationPourParticipantForm(forms.ModelForm):
         exclude = ('participant', 'seance', 'inscription_date')
 
 
-class InscriptionFormationPourParticipant(LoginRequiredMixin, DeleteView, CreateView, SingleObjectMixin):
+class InscriptionFormationPourParticipant(LoginRequiredMixin, CreateView, DeleteView, SingleObjectMixin):
     model = Inscription
     pk_url_kwarg = 'formation_id'
     context_object_name = "formation"
@@ -40,30 +41,27 @@ class InscriptionFormationPourParticipant(LoginRequiredMixin, DeleteView, Create
         )
 
     def post(self, request, *args, **kwargs):
-        seances_liste_pour_inscription_a_creer = []
-        inscriptions_liste_a_supprimer = []
         seance_list_apres_post = self.request.POST.getlist('seance')
+        seance_uuid_list_apres_post = []
+        for seance in seance_list_apres_post:
+            seance_uuid_list_apres_post.append(UUID(seance))
         inscriptions_existantes_liste_avant_post = Inscription.objects.filter(
             participant__user=request.user,
             seance__formation=self.get_object()
         )
-        for inscription_existante in inscriptions_existantes_liste_avant_post:
-            if str(inscription_existante.seance_id) not in seance_list_apres_post:
-                inscriptions_liste_a_supprimer.append(inscription_existante)
-        for seance_id in seance_list_apres_post:
-            if not Inscription.objects.filter(participant__user=request.user, seance__id=seance_id).exists():
-                seances_liste_pour_inscription_a_creer.append(seance_id)
+        seance_to_delete = inscriptions_existantes_liste_avant_post.exclude(seance__id__in=seance_uuid_list_apres_post)
+        seance_to_create = set(seance_uuid_list_apres_post) - set(
+            inscriptions_existantes_liste_avant_post.values_list('seance_id', flat=True))
 
-        if inscriptions_liste_a_supprimer:
-            self.delete(request, inscriptions_liste=inscriptions_liste_a_supprimer)
-        if seances_liste_pour_inscription_a_creer:
-            self.create(request, seances_liste=seances_liste_pour_inscription_a_creer)
+        self.delete(request, seance_to_delete)
+        self.create(request, seance_to_create)
+
         return redirect(
             self.get_success_url()
         )
 
-    def delete(self, request, *args, **kwargs):
-        for inscription in kwargs['inscriptions_liste']:
+    def delete(self, request, to_delete):
+        for inscription in to_delete:
             inscription.delete()
             messages.success(
                 request,
